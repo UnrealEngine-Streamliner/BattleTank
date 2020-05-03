df6f7b0b013198dd21e4ef346462afe8cb0b1e8e
Streamliner <thisisstreamliner@gmail.com>
2020-05-03 15:12:07 +0300

    Overriding BeginPlay in TankPlayerController to print log message (Using virtual and override methods)

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,20 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not Possesing a Tank"));
+	} 
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Possesing: %s"), *ControlledTank->GetName());
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
